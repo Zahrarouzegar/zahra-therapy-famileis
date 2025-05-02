64aff5cf0fca76ac340b443f138226c32cb601bf
Zahrarouzegar <zhroozegar@gmail.com>
2025-05-02 13:22:07 -0400

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function () {
+  console.log("فوتر لود شد.");
+});
---
+++ header.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function () {
+  console.log("هدر لود شد.");
+});
